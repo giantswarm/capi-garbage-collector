f6f9f2d3cf5606754882620d218b07b66b782efb
Marcus Noble <AverageMarcus@users.noreply.github.com>
2022-11-21 09:44:07 +0000

    Added experimental api to scheme (#8)

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -7,6 +7,10 @@ and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0
 
 ## [Unreleased]
 
+### Fixed
+
+- Add experimental api to scheme
+
 ## [0.1.1] - 2022-11-21
 
 ### Fixed
--- main.go
+++ main.go
@@ -27,8 +27,8 @@ import (
 	"k8s.io/apimachinery/pkg/runtime"
 	utilruntime "k8s.io/apimachinery/pkg/util/runtime"
 	clientgoscheme "k8s.io/client-go/kubernetes/scheme"
-	capa "sigs.k8s.io/cluster-api-provider-aws/api/v1beta1"
 	capi "sigs.k8s.io/cluster-api/api/v1beta1"
+	capiexp "sigs.k8s.io/cluster-api/exp/api/v1beta1"
 	ctrl "sigs.k8s.io/controller-runtime"
 	"sigs.k8s.io/controller-runtime/pkg/healthz"
 	"sigs.k8s.io/controller-runtime/pkg/log/zap"
@@ -45,7 +45,7 @@ var (
 func init() {
 	utilruntime.Must(clientgoscheme.AddToScheme(scheme))
 	utilruntime.Must(capi.AddToScheme(scheme))
-	utilruntime.Must(capa.AddToScheme(scheme))
+	utilruntime.Must(capiexp.AddToScheme(scheme))
 
 	// +kubebuilder:scaffold:scheme
 }
